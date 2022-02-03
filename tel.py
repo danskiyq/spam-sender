5f40a2d01f729c5315735537a725bd149d9842dd
danskiyq <poprotskyyda@gmail.com>
2022-02-03 22:31:14 +0200

    V0.1

--- tel.py
+++ tel.py
@@ -1,18 +1,18 @@
 from telethon import TelegramClient
-from time import time, sleep
+from time import time
 import telethon.sync
 import asyncio
 
 
 class TimedSpam:
     """ Class sends message with given frequency in seconds"""
-    def __init__(self, msg: str, ch_list: list, fq, send_immediately=False):
+    def __init__(self, msg: str, ch_list: list, fq: int, send_immediately=False):
         self.message = msg
         self.channels_list = ch_list
         self.frequency = fq
         self.send_immediately = send_immediately
 
-    async def send_spam(self):
+    async def send_spam(self) -> None:
         if not self.send_immediately:  # check if message needs to be sent immediately
             await asyncio.sleep(self.frequency)
 
@@ -23,21 +23,27 @@ class TimedSpam:
             await asyncio.sleep(self.frequency - (time() - start_time))
 
 
+def create_timed_spam() -> TimedSpam:
+    message = input('Message text:')
+    frequency = int(input('Set frequency in seconds (Leave blank for 1 hour): '))
+    send_immediately = (True if input('Do you want to send messages immediately? (Y/N):')
+                        in ('y', 'Y', 'Yes', 'yes', 'YES') else False)
+    channels_list = [name.strip() for name in open('venv/channels_list', 'r')]
+
+    if not frequency:
+        frequency = 3600
+
+    return TimedSpam(message, channels_list, fq=frequency, send_immediately=send_immediately)
+
+
 if __name__ == '__main__':
     api_id = 16734569
     api_hash = '8c137eecf2abd641f472740daf3ab0fa'
 
-    message = input('Message text:')
-    frequency = int(input('Set frequency in seconds (Leave blank for 1 hour): '))
-    send_immediately = (True if input('Do you want to send messages immediately? (Y/N):') in ('y', 'Y', 'Yes', 'yes', 'YES') else False)
-    if not frequency:
-        frequency = 3600
+    timed_spam = create_timed_spam()
 
-    client = TelegramClient('@zlatachkka', api_id, api_hash).start()
+    client = TelegramClient('anon', api_id, api_hash).start()
     client.get_dialogs()
-    channels_list = [name.strip() for name in open('venv/channels_list', 'r')]
-
-    timed_spam = TimedSpam(message, channels_list, fq=frequency)
 
     loop = asyncio.get_event_loop()
     tasks = asyncio.wait([timed_spam.send_spam()])
